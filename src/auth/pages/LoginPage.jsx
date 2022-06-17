1c45cb60b92fe4427d4fbccd2f8ce572ec7820bc
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2022-06-17 12:23:02 -0500

    New featuresparkles :sparkles: add Login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,24 +1,13 @@
-import { Grid, TextField, Typography } from '@mui/material'
+import { Google } from '@mui/icons-material'
+import { Button, Grid, TextField, Typography, Link } from '@mui/material'
 import React from 'react'
+import { Link as RouterLink } from 'react-router-dom'
+import { AuthLayout } from '../layout/AuthLayout'
 
 export const LoginPage = () => {
   return (
-    <Grid
-      container
-      direction="column"
-      spacing={0}
-      justifyContent="center"
-      alignItems="center"
-      sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
-    >
-      <Grid item
-        className="box-shadow"
-        xs={3}
-        sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}
-        >
-        <Typography variant="h5" sx={{ mb:1 }}>Login</Typography>
-
-        <form>
+    <AuthLayout title="Login">
+      <form>
           <Grid container>
             <Grid item xs={12} sx={{ mt: 1}}>
               <TextField
@@ -37,11 +26,37 @@ export const LoginPage = () => {
                 fullWidth
                 />
             </Grid>
+
+            <Grid container spacing={2}  sx={{ mt: 2, mb: 2}}>
+              <Grid item xs={12} sm={6}>
+                <Button variant="contained" fullWidth>
+                  Login
+                </Button>
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+                <Button variant="contained" fullWidth>
+                  <Google />
+                  <Typography sx={{ ml: 1 }}>Google</Typography>
+                </Button>
+              </Grid>
+
+            </Grid>
+
+
+            <Grid container direction="row" justifyContent="end">
+              <Link component={ RouterLink} color="primary" to="/auth/register">
+                Crear una cuenta
+              </Link>
+
+            </Grid>
+
+
           </Grid>
         </form>
-      </Grid>
 
-    </Grid>
+    </AuthLayout>
+
   )
 }
 
