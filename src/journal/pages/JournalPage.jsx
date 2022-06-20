1e0fbf9417599fa99776fe5ec6c00c6f17208fdf
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2022-06-19 21:22:05 -0500

    Add a button plus journal

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,8 @@
 import { NothingSelectedView } from '../views/NothingSelectedView.jsx'
 import { JournalLayout } from '../layout/JournalLayout.jsx'
 import { NoteView } from '../views/NoteView.jsx'
+import { IconButton } from '@mui/material'
+import { AddOutlined } from '@mui/icons-material'
 
 export const JournalPage = () => {
   return (
@@ -8,11 +10,23 @@ export const JournalPage = () => {
       {/* <Typography variant="body1">
         Ullamco nostrud reprehenderit nostrud non nostrud dolor elit aliqua exercitation velit labore elit.
       </Typography> */}
-      {/* <NothingSelectedView /> */}
+      <NothingSelectedView />
 
-      <NoteView />
+      {/* <NoteView /> */}
 
-
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 35}}/>
+      </IconButton>
 
     </JournalLayout>
 
