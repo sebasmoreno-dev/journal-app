9c5e58f8bc6d80f90daaf2e243b3b1e4697509eb
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2022-07-21 15:54:23 -0500

    cloudinary SDK - Delete image

--- tests/helpers/fileUpload.test.js
+++ tests/helpers/fileUpload.test.js
@@ -1,5 +1,12 @@
+import { v2 as cloudinary } from 'cloudinary';
 import { fileUpload } from './../../src/helpers/fileUpload';
 
+cloudinary.config({
+  cloud_name: 'duf3yksmd',
+  api_key: '941558362379815',
+  api_secret: '-lGfpGqWTdn4LJ_Qip42wYHcWc0',
+  secure: true,
+})
 
 describe('File Upload', () => {
 
@@ -12,5 +19,23 @@ describe('File Upload', () => {
 
     const url = await fileUpload(file);
     expect( typeof url ).toBe('string');
-  })
-})
+
+    const segments = url.split('/');
+    const imageId = segments[segments.length - 1].replace('.jpg', '');
+
+    const cloudResp = await cloudinary.api.delete_resources(['journal-app/' + imageId], {
+      resource_type: 'image',
+    });
+    console.log(cloudResp);
+
+  });
+
+  test('Debe de retornar null', async () => {
+    const file = new File([], 'test.jpg');
+
+    const url = await fileUpload(file);
+    expect( url ).toBe(null);
+  });
+
+
+});
