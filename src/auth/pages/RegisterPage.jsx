e2eec4f08019ddc7547046f18b175a01953560d1
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2022-08-29 22:54:30 -0500

    Debugging registerPage

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,141 +1,56 @@
-import { Button, Grid, TextField, Typography, Link, Alert } from '@mui/material'
-import React, { useMemo, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Link as RouterLink } from 'react-router-dom'
-import { useForm } from '../../hooks'
-import { startRegisterWithEmailAndPassword } from '../../store/auth/thunks'
-import { AuthLayout } from '../layout/AuthLayout'
-
-const formData = {
-  email: "",
-  password: "",
-  displayName: "",
-}
-
-//objeto que contiene las validaciones
-const formValidations = {
-  //arreglo de de dos valores
-  email: [(value) => value.includes('@'), 'El correo debe de tener un @'],
-  password: [(value) => value.length >= 6, 'El password debe tener más de 6 letras'],
-  displayName: [(value) => value.length >= 1, 'El nombre es obligatorio'],
-}
-
+import { Link as RouterLink } from "react-router-dom";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { Google } from "@mui/icons-material";
+import { AuthLayout } from "../layout/AuthLayout";
 
 export const RegisterPage = () => {
-
-  const dispatch = useDispatch();
-
-  const [formSubmitted, setFormSubmitted] = useState(false);
-
-  const { status, errorMessage} = useSelector((state) => state.auth);
-  const isCheckingAuthentication = useMemo(() => status === 'checking', [status]);
-
-  const {
-    formState, displayName, email, password, onInputChange,
-    isFormValid, displayNameValid, emailValid, passwordValid,
-    } = useForm(formData, formValidations);
-
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    setFormSubmitted(true);
-
-    if (!isFormValid) return;
-    dispatch(startRegisterWithEmailAndPassword(formState));
-  }
-
-
   return (
     <AuthLayout title="Crear cuenta">
-      <h1>FormValid: { isFormValid ? 'Valido' : 'Incorrecto'}</h1>
-      <form
-        onSubmit={onSubmit}
-        className='animate__animated animate__fadeIn animate__faster'
-      >
-          <Grid container>
-            <Grid item xs={12} sx={{ mt: 1}}>
-              <TextField
-                label="Nombre completo"
-                type="text"
-                placeholder="Tu nombre"
-                fullWidth
-                name="displayName"
-                value={displayName}
-                onChange={onInputChange}
-                error={!!displayNameValid && formSubmitted}
-                helperText={displayNameValid }
-                />
-            </Grid>
-
-            <Grid item xs={12} sx={{ mt: 1}}>
-              <TextField
-                label="Correo"
-                type="email"
-                placeholder="Correo@google.com"
-                fullWidth
-                name="email"
-                value={email}
-                onChange={onInputChange}
-                error={!!emailValid && formSubmitted}
-                helperText={emailValid }
-                />
-            </Grid>
-
-            <Grid item xs={12} sx={{ mt: 2}}>
-              <TextField
-                label="Contraseña"
-                type="password"
-                placeholder="Contraseña"
-                fullWidth
-                name="password"
-                value={password}
-                onChange={onInputChange}
-                error={!!passwordValid && formSubmitted}
-                helperText={passwordValid }
-                />
-            </Grid>
-
-            <Grid container spacing={2}  sx={{ mt: 2, mb: 2}}>
-              <Grid
-                item
-                xs={12}
-                /*If it is falsy, then it will not display the element. */
-                display={ !!errorMessage ? '' : 'none'}
-              >
-                <Alert severity="error">
-                  {errorMessage}
-                </Alert>
-              </Grid>
-
-              <Grid item xs={12}>
-                <Button
-                  disabled={isCheckingAuthentication}
-                  type="submit"
-                  variant="contained"
-                  fullWidth>
-                  Crear cuenta
-                </Button>
-              </Grid>
-
-
-
-            </Grid>
-
-
-            <Grid container direction="row" justifyContent="end">
-              <Typography sx={{ mr: 1}} >¿Ya tienes cuenta?</Typography>
-              <Link component={ RouterLink} color="primary" to="/auth/login">
-                Ingresar
-              </Link>
-
-            </Grid>
-
-
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Nombre completo"
+              type="text"
+              placeholder="Nombre completo"
+              fullWidth
+            />
           </Grid>
-        </form>
 
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Correo"
+              type="email"
+              placeholder="correo@google.com"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Contraseña"
+              type="password"
+              placeholder="Contraseña"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12}>
+              <Button variant="contained" fullWidth>
+                Crear cuenta
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" justifyContent="end">
+            <Typography sx={{ mr: 1 }}>¿Ya tienes cuenta?</Typography>
+            <Link component={RouterLink} color="inherit" to="/auth/login">
+              ingresar
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
     </AuthLayout>
-
-  )
-}
-
+  );
+};
